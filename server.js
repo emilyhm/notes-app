db32b85d8413e49061d5da7045cdf36d8b0067e5
Your Nameclear <you@example.com>
2019-10-17 12:15:15 -0400

    second commit

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ if (process.env.NODE_ENV === "production") {
   uri = process.env.ATLAS_URI;
 } else {  
   // localhost
-  uri = process.env.LOCAL_URI  
+  // uri = process.env.LOCAL_URI  
+  uri = process.env.ATLAS_URI;
 }
 
 // database connection ppol
